a515e19d5efb1b9c12d46146ced8740ded69a520
Doug Bostick <dougbostick@Dougs-MacBook.lan>
2023-09-13 16:17:01 -0400

    fixed random letter not finding a

--- src/gameFunctions/generateLetter.js
+++ src/gameFunctions/generateLetter.js
@@ -1,6 +1,6 @@
 export const generateLetter = () => {
   const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  const randomNum = Math.ceil(Math.random() * 25);
+  const randomNum = Math.floor(Math.random() * 26);
   const randomLetter = alphabet[randomNum];
   console.log(randomLetter);
   return randomLetter;
