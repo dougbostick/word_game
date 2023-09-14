2f8d71001fdea3d50117334434923bc4dbb32073
Doug Bostick <dougbostick@Dougs-MacBook.lan>
2023-09-13 23:27:26 -0400

    added timer

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [timer, setTimer] = useState(60);
+  const [gameStatus, setGameStatus] = useState(true);
   // const res = [];
   // for (let i = 0; i < 100; i++) {
   //   const letter = generateWordLength();
@@ -17,18 +18,31 @@ function App() {
   // console.log(res.sort());
 
   // setTimer(countDown(timer));
-  const countDown = (timer) => {
-    console.log(timer);
+  // const countDown = (timer) => {
+  //   console.log('running');
+  //   while (timer > 0) {
+  //     setTimer(timer - 1);
+  //   }
+  // };
+  const handleClick = () => {
     if (timer > 0) {
       setTimer(timer - 1);
+    } else {
+      setGameStatus(false);
     }
   };
 
   useEffect(() => {
-    setTimeout(countDown, 1000);
-  }, []);
+    setTimeout(handleClick, 1000);
+  });
 
-  return <div className="App">{timer}</div>;
+  return (
+    <div className="App">
+      <div>{timer}</div>
+      {/* <button onClick={() => handleClick()}>timer</button> */}
+      <div>{gameStatus ? 'GAME ON' : 'GAME OVER'}</div>
+    </div>
+  );
 }
 
 export default App;
